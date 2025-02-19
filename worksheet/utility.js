6cac74f0a4043184d21cb57f3bfe0823e8c842d4
gabtendelski <99609208+gabtendelski@users.noreply.github.com>
2025-02-19 13:29:41 +0100

    Update utility.js

--- worksheet/utility.js
+++ worksheet/utility.js
@@ -11,6 +11,40 @@ export function subtract(a, b) {
  * "export" to the function definition (as seen in the
  * subtract example above)
  */
-function add(a, b) {
+export function add(a, b) {
   return a + b;
+}
+
+export function compare(a, b){
+  if(a.length === b.length){
+    for(let i = 0; i < a.length; i++){
+      if (a[i] != b[i]){
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+  
+}
+
+export function largest(arr){
+  if(Array.isArray(arr) && arr.length > 0){
+    return Math.max(...arr);
+  }
+  return null;
+}
+
+export function zeroest(arr){
+  let closest;
+  arr.forEach(num => {
+    if (closest != undefined){
+      if(closest > Math.sqrt(num*num)){
+        closest = num;
+      }
+    } else {
+      closest = Math.sqrt(num*num);
+    }
+  });
+  return closest;
 }
